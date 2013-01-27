c9df7c321dc9230270ed9c621678a12949cddab2
Marc Rudolph Jacinto <jacinto.marc@gmail.com>
2013-01-27 22:29:26 +0800

    This codes are for the capstone project of Jacinto, Gonzaga, Abapo and Bagoyo

---
+++ SRIS/membersite_config.php
@@ -0,0 +1,24 @@
+<?PHP
+require_once("fg_membersite.php");
+
+$fgmembersite = new FGMembersite();
+
+//Provide your site name here
+$fgmembersite->SetWebsiteName('user11.com');
+
+//Provide the email address where you want to get notifications
+$fgmembersite->SetAdminEmail('user11@user11.com');
+
+//Provide your database login details here:
+//hostname, user name, password, database name and table name
+//note that the script will create the table (for example, fgusers in this case)
+//by itself on submitting register.php for the first time
+$fgmembersite->InitDB(/*hostname*/'localhost',
+                      /*database name*/'sris',
+                      /*table name*/'client');
+
+//For better security. Get a random string from this link: http://tinyurl.com/randstr
+// and put it here
+$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+
+?>
